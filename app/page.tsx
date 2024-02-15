ad07cad48609e29034adf9739cee4cf2eee6435d
yuhijackman <yuuhiandlisa@gmail.com>
2024-02-15 22:53:58 +0900

    Create Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">hello</main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Navbar />
+    </main>
   );
 }
